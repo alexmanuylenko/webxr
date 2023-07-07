645f6fdfaeef1ce239b6f862e186090cdbc8521b
Alexey Manuylenko <alexeyma@mail.ru>
2023-07-07 20:44:52 +0200

    Commented webpack.prod.js

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,13 +1,27 @@
+// Файл конфигурации сборщика Webpack для релизной версии (production)
+// Здесь все работает как надо, поэтому вряд ли понадобится что-то менять.
+
+// Инструмент для слияния конфигураций (JS-объектов) - merge:
 const { merge } = require('webpack-merge')
+
+// Общая конфигурация из файла './webpack.common.js':
 const commonConfiguration = require('./webpack.common.js')
+
+// Плагин очистки проекта от отладочной информации ("дебажного мусора"):
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-module.exports = merge(
-    commonConfiguration,
+// Сливаем с общей для всех конфигурацией:
+module.exports = merge( // Слияние
+    commonConfiguration, // Конфигурация из './webpack.common.js'
+    
+    // Production-конфигурация:
     {
-        mode: 'production',
+        mode: 'production', // Релизный режим (production)
+        
+        // Подключаем плагин:
         plugins:
         [
+            // Очистка проекта от отладочной информации ("дебажного мусора"):
             new CleanWebpackPlugin()
         ]
     }
